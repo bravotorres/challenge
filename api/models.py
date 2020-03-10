39031dce9e5e5d1c5116e643c1902b8ec9528008
Alejandro Bravo Torres <bravotorres.alejandro@gmail.com>
2020-03-09 22:16:26 -0600

    update code

---
+++ api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+"""
+Definitions:
+Album:
+- Title
+- Soft cover / Hard cover
+- Photos
+- Date of creation
+
+Photo:
+- URL of the image
+- Caption
+- Date of creation
+
+Constraints:
+- One Album has many photos and the photo only belongs to one album
+"""
+
+
+class Album(models.Model):
+    class Cover(models.TextChoices):
+        SOFT = 'Softcover'
+        HARD = 'Hardcover'
+
+    title = models.CharField(db_column='Title', max_length=128, blank=False, null=False)
+    cover_type = models.CharField(db_column='CoverType', max_length=64, choices=Cover.choices, default=Cover.SOFT)
+    creation_at = models.DateTimeField(db_column='Created', auto_now_add=True)
+    updated_at = models.DateTimeField(db_column='LastUpdated', auto_now=True)
+
+    class Meta:
+        db_table = 'Album'
+
+
+class Photo(models.Model):
+    url = models.CharField(db_column='URL', max_length=256, blank=False, null=False)
+    caption = models.CharField(db_column='Caption', max_length=128)
+    created_at = models.DateTimeField(db_column='Created', auto_now_add=True)
+    updated_at = models.DateTimeField(db_column='LastUpdated', auto_now=True)
+    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Photo'
---
+++ challenge/__init__.py
@@ -0,0 +1,4 @@
+import os
+import dotenv
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
