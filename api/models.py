d702d937c4b76c87e8d708644755c6efa2595d5b
Alejandro Bravo <bravotorres.alejandro@gmail.com>
2020-03-17 09:00:55 -0600

    Change permissions files.
    Add migration

--- api/models.py
+++ api/models.py
@@ -38,7 +38,7 @@ class Album(models.Model):
 class Photo(models.Model):
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
     url = models.CharField(db_column='URL', max_length=256, blank=False, null=False)
-    image = models.ImageField(upload_to=default_storage.path('photos'))
+    image = models.ImageField(upload_to='PHOTOS/%Y/%m/%d')
     caption = models.CharField(db_column='Caption', max_length=128)
     created_at = models.DateTimeField(db_column='Created', auto_now_add=True)
     updated_at = models.DateTimeField(db_column='LastUpdated', auto_now=True)
